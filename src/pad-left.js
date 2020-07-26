06da189a63221422dd1e221514c2f59fea10e27e
Bong Lee <bongster228@github.com>
2020-07-26 01:36:57 -0700

    WIP: Cannot get coverage quite there

--- src/pad-left.js
+++ src/pad-left.js
@@ -6,15 +6,15 @@ export default padLeft
  * This method will pad the left of the given string by
  * the given size with the given character
  *
- * @param {String} str - The string to pad
+ * @param {String} stri - The string to pad
  * @param {Number} size - The total size to pad
  * @param {String} padWith - The character to use for padding
  * @return {String} - The padded string
  */
-function padLeft(str, size, padWith) {
-  if (size <= str.length) {
-    return str
+function padLeft(stri, size, padWith) {
+  if (size <= stri.length) {
+    return stri
   } else {
-    return Array(size - str.length + 1).join(padWith || '0') + str
+    return Array(size - stri.length + 1).join(padWith || '0') + stri
   }
 }
